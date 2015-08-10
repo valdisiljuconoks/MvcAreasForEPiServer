84c5d889b13d9937c56aec581ff8888fb95a86a0
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2015-08-10 12:05:52 +0300

    Added support for areas as EPiServer sites

--- EPiServer.MvcAreas/AreaCollection.cs
+++ EPiServer.MvcAreas/AreaCollection.cs
@@ -1,8 +1,13 @@
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace EPiServer.MvcAreas
 {
     public class AreaCollection : Collection<Area>
     {
+        public bool Contains(string areaName)
+        {
+            return this.FirstOrDefault(a => a.Name.Equals(areaName)) != null;
+        }
     }
 }
---
+++ EPiServer.MvcAreas/SwitchToAreaAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using EPiServer.Web;
+
+namespace EPiServer.MvcAreas
+{
+    public class SwitchToAreaAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            // if there is an area with the same name of the site - switch to that area
+            if (AreaTable.Areas.Contains(SiteDefinition.Current.Name))
+            {
+                filterContext.RouteData.DataTokens["area"] = SiteDefinition.Current.Name;
+            }
+        }
+    }
+}
