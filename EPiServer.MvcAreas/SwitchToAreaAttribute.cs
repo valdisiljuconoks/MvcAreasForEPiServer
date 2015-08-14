d0339e799650b4e6148a4aec289dc13ad0d68aab
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2015-08-15 00:04:59 +0300

    Added check for ContentController. Need to alter area only then.

--- EPiServer.MvcAreas/SwitchToAreaAttribute.cs
+++ EPiServer.MvcAreas/SwitchToAreaAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Mvc;
 using EPiServer.Web;
+using EPiServer.Web.Mvc;
 
 namespace MvcAreasForEPiServer
 {
@@ -9,10 +10,25 @@ namespace MvcAreasForEPiServer
         {
             base.OnActionExecuting(filterContext);
 
-            // if there is an area with the same name of the site - switch to that area
-            if (AreaTable.Areas.Contains(SiteDefinition.Current.Name))
+            /*
+
+            We need test if current controller is actually a content controller - was request made against *normal* page,
+            or this is other controller - like CMS UI.
+
+            Check is needed - because there are weird side-effects if we alter area DataToken, and CMS module controllers start to render
+            relative Urls in the menu - some really routings get generated, like /cms/cms?moduleArea=AddOn.
+
+            Easiest way was to alter area token only then, when this is really necessary - request hits real content controller.
+
+            */
+
+            if (filterContext.Controller.GetType().IsSubclassOf(typeof (ActionControllerBase)))
             {
-                filterContext.RouteData.DataTokens["area"] = SiteDefinition.Current.Name;
+                // if there is an area with the same name of the site - switch to that area
+                if (AreaTable.Areas.Contains(SiteDefinition.Current.Name))
+                {
+                    filterContext.RouteData.DataTokens["area"] = SiteDefinition.Current.Name;
+                }
             }
         }
     }
