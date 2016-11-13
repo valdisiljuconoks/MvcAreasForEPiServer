55fdc0d4cbadb3bc4b7a44b147e7805235cf7ef3
valdisiljuconoks <valdis@VALDIS-PC>
2016-11-13 02:42:02 +0200

    Initial support for EPiServer 10

--- src/MvcAreasForEPiServer/AddMvcAreasSupportModule.cs
+++ src/MvcAreasForEPiServer/AddMvcAreasSupportModule.cs
@@ -8,30 +8,33 @@ using InitializationModule = EPiServer.Web.InitializationModule;
 
 namespace MvcAreasForEPiServer
 {
-    [ModuleDependency(typeof (InitializationModule))]
+    [ModuleDependency(typeof(InitializationModule))]
     public class AddMvcAreasSupportModule : IInitializableModule
     {
         public void Initialize(InitializationEngine context)
         {
-            if (AreaConfiguration.Settings.EnableAreaDetectionByController)
+            if(AreaConfiguration.Settings.EnableAreaDetectionByController)
             {
                 GlobalFilters.Filters.Add(ServiceLocator.Current.GetInstance<DetectAreaAttribute>());
             }
 
-            if (AreaConfiguration.Settings.EnableAreaDetectionBySite)
+            if(AreaConfiguration.Settings.EnableAreaDetectionBySite)
             {
                 GlobalFilters.Filters.Add(ServiceLocator.Current.GetInstance<SwitchToAreaAttribute>());
             }
 
-            ContentRoute.RoutingContent += OnRoutingContent;
+            var emitter = context.Locate.Advanced.GetInstance<IContentRouteEvents>();
+            emitter.RoutingContent += OnRoutingContent;
         }
 
-        public void Uninitialize(InitializationEngine context) {}
+        public void Uninitialize(InitializationEngine context) { }
 
         private void OnRoutingContent(object sender, RoutingEventArgs e)
         {
             PartialViewsInAreasRegistrar.Register(new HttpContextWrapper(HttpContext.Current));
-            ContentRoute.RoutingContent -= OnRoutingContent;
+
+            var emitter = ServiceLocator.Current.GetInstance<IContentRouteEvents>();
+            emitter.RoutingContent -= OnRoutingContent;
         }
     }
 }
--- tests/AreasResearch/SampleSiteFeature.cs
+++
@@ -1,8 +0,0 @@
-﻿using FeatureSwitch;
-using FeatureSwitch.EPiServer.Strategies;
-
-namespace AreasResearch
-{
-    [EPiServerSite(Key = "AreaSite1, AreaSite2")]
-    public class SampleSiteFeature : BaseFeature {}
-}
